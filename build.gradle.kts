b49b2ca6b78c77ff45c5a0b47eca4cef6c4c3502
EvanLuo42 <ziyun.luo@icloud.com>
2022-01-18 11:27:30 +0800

    build: Add essential packages for Jetpack Compose
    Add UI Support
    Add Foundation Support
    Add Material Design Icon
    Add Activity
    Add ViewModel
    Add Data Support

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,30 @@ repositories {
 }
 
 dependencies {
+    implementation("androidx.compose.ui:ui:1.0.5")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.0.5")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:1.0.5")
+    // Material Design
+    implementation("androidx.compose.material:material:1.0.5")
+    // Material design icons
+    implementation("androidx.compose.material:material-icons-core:1.0.5")
+    implementation("androidx.compose.material:material-icons-extended:1.0.5")
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.3.1")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+    // Integration with observables
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.5")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.5")
+
     implementation(compose.desktop.currentOs)
     implementation("org.ktorm:ktorm-core:3.4.1")
     val miraiVersion = "2.9.2"
     api("net.mamoe", "mirai-core-api", miraiVersion)     // 编译代码使用
     runtimeOnly("net.mamoe", "mirai-core", miraiVersion) // 运行时使用
+    implementation("io.coil-kt:coil-compose:1.4.0") // Fetch image from network
 }
 
 tasks.withType<KotlinCompile> {
